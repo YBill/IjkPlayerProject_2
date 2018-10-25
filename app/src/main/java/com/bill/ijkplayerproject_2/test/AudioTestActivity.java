5fc52b3c331188d5ebb16b405ff721f644eaa992
Bill <ywbandroid@gmail.com>
2018-10-25 19:09:18 +0800

    创建解码器

--- app/src/main/java/com/bill/ijkplayerproject_2/test/AudioTestActivity.java
+++ app/src/main/java/com/bill/ijkplayerproject_2/test/AudioTestActivity.java
@@ -9,12 +9,15 @@ import com.bill.ijkplayerproject_2.R;
 
 import java.io.IOException;
 
+import tv.danmaku.ijk.media.exo.IjkExoMediaPlayer;
+import tv.danmaku.ijk.media.player.AbstractMediaPlayer;
+import tv.danmaku.ijk.media.player.AndroidMediaPlayer;
 import tv.danmaku.ijk.media.player.IMediaPlayer;
 import tv.danmaku.ijk.media.player.IjkMediaPlayer;
 
 public class AudioTestActivity extends AppCompatActivity {
 
-    private IjkMediaPlayer ijkMediaPlayer;
+    private AbstractMediaPlayer mPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,9 @@ public class AudioTestActivity extends AppCompatActivity {
     }
 
     public void handlePlayAudio(View view) {
-        createPlayer();
+//        createIjkPlayer();
+        createExoPlayer();
+//        createMediaPlayer();
     }
 
     @Override
@@ -32,28 +37,51 @@ public class AudioTestActivity extends AppCompatActivity {
         release();
     }
 
-    private void createPlayer() {
-        ijkMediaPlayer = new IjkMediaPlayer();
-        ijkMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    private void createIjkPlayer() {
+        mPlayer = new IjkMediaPlayer();
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         try {
-            ijkMediaPlayer.setDataSource("https://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f30.mp4");
+            mPlayer.setDataSource("https://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f30.mp4");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ijkMediaPlayer.prepareAsync();
-        ijkMediaPlayer.setOnCompletionListener(new IMediaPlayer.OnCompletionListener() {
+        mPlayer.prepareAsync();
+    }
+
+    private void createExoPlayer() {
+        mPlayer = new IjkExoMediaPlayer(this);
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mPlayer.setDataSource("https://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f30.mp4");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mPlayer.prepareAsync();
+        mPlayer.start();
+    }
+
+    private void createMediaPlayer() {
+        mPlayer = new AndroidMediaPlayer();
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        try {
+            mPlayer.setDataSource("https://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f30.mp4");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mPlayer.prepareAsync();
+        mPlayer.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
             @Override
-            public void onCompletion(IMediaPlayer mp) {
-                release();
+            public void onPrepared(IMediaPlayer mp) {
+                mPlayer.start();
             }
         });
     }
 
     private void release() {
-        if (ijkMediaPlayer != null) {
-            ijkMediaPlayer.stop();
-            ijkMediaPlayer.release();
-            ijkMediaPlayer = null;
+        if (mPlayer != null) {
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
         }
     }
 
